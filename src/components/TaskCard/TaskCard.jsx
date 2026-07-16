7e3a79b3e6a6fe8983a5db962bf453b1623efca8
Vivian <vivianqueiroz_@outlook.com>
2026-07-15 20:43:43 -0400

    fix: correctly format the task date and handle empty dates

--- src/components/TaskCard/TaskCard.jsx
+++ src/components/TaskCard/TaskCard.jsx
@@ -1,13 +1,15 @@
 
 
 export function TaskCard({task, onDelete, onToggleDone}) {
-  
+  const MESES = ['jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez']
+  const [year, month, day] = task.date ? task.date.split('-') : ['', '', '']
+  const dataFormatada = task.date ? `${day} ${MESES[month - 1]} ${year}` : ''
   return(
     <div className="">
       <h3>{task.title}</h3>
       <p>{task.category}</p>
       <p>{task.priority}</p>
-      <p>{task.date}</p>
+      <p>{dataFormatada}</p>
       <input type="checkbox" name="" id="" checked={task.done} onChange={() => onToggleDone(task.id)} />
       <button onClick={() => onDelete(task.id)}>X</button>
     </div>
